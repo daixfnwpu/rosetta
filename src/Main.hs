bb2aea37222b4cdc6eb0b964c2366193fc42365c
daixfnwpu <daixfnwpu@gmail.com>
2022-05-31 22:32:19 +0800

    finish the 24 solved games

--- src/Main.hs
+++ src/Main.hs
@@ -6,7 +6,9 @@ import FifthPuzzleGame
 import TwoZeroFourEightGame
 import TwentityGame
 import TwentityFour
+import TwentitySolveGame
 --main = FifthPuzzleGame.runMain
 --main = TwoZeroFourEightGame.runMain
 --main = TwentityGame.runMain
-main = TwentityFour.runMain
+--main = TwentityFour.runMain
+main = TwentitySolveGame.runMain
