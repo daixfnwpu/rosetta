a475974fd7d7ada3c6909f16a2ee2fd5ba2ab86f
daixfnwpu <daixfnwpu@gmail.com>
2022-05-31 22:04:32 +0800

    finish the 24 game

--- src/Main.hs
+++ src/Main.hs
@@ -5,6 +5,8 @@ import qualified  ThroundPrisoners (runMain)
 import FifthPuzzleGame
 import TwoZeroFourEightGame
 import TwentityGame
+import TwentityFour
 --main = FifthPuzzleGame.runMain
 --main = TwoZeroFourEightGame.runMain
-main = TwentityGame.runMain
+--main = TwentityGame.runMain
+main = TwentityFour.runMain
